21ae36f6e64321aa058cca22778f24e51ae9bf2d
Jose Luis <jlrp09102005@gmail.com>
2025-12-04 10:31:38 +0100

    new activity

---
+++ Jardineria/CTE-recursiveCTE.sql
@@ -0,0 +1,6 @@
+-- Active: 1763026326945@@127.0.0.1@3306@jardineria
+-----------------------
+-- Consulta 1 
+-----------------------
+
+-- Muestra el nombre de los empleados y el total de ventas que han realizado en 2023, pero solo si superan las 50 unidades vendidas.
