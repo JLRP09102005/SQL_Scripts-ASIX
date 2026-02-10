30e4bc0e283994f8edee4847bfdc99a08f71ca25
Jose Luis <jlrp09102005@gmail.com>
2026-02-10 12:47:29 +0100

    Save

--- WEC/functions.sql
+++ WEC/functions.sql
@@ -3,21 +3,12 @@ USE WEC;
 
 DELIMITER //
 
-CREATE PROCEDURE MaxLimit ( numToCheck INT, maxNumber INT, customErrorMessage VARCHAR(100) )
-
+CREATE FUNCTION UnderMaxLimit ( numToCheck INT, maxNumber INT )
+RETURNS TINYINT(1)
 COMMENT='Check if the position to insrrt is unique per race'
-
+DETERMINISTIC
 BEGIN
-
-    IF new_vehicles_num > NEW.max_vehicles THEN
-        SIGNAL SQLSTATE '45000'
-        IF customErrorMessage IS NULL OR customErrorMessage = '' THEN
-            SET MESSAGE_TEXT = 'Number exceed the limit';
-        ELSE
-            SET MESSAGE_TEXT = customErrorMessage;
-        END IF;
-    END IF;
-
+    RETURN (numToCheck < maxNumber )
 END;
 
 DELIMITER ;
--- WEC/trigger_valid_inscription.sql
+++ WEC/trigger_valid_inscription.sql
@@ -19,20 +19,10 @@ BEGIN
     JOIN pilots p ON p.id_pilot = pilins.id_pilot
     WHERE pilins.id_vehicle = NEW.id_vehicle AND pilins.id_team = NEW.id_team AND pilins.id_race = NEW.id_race;
 
-    SELECT MaxLimit(new_vehicles_num, NEW.max_vehicles, 'The vehicles number exceed the maximum allowed');
-    SELECT MaxLimit(new_pilots_num, NEW.max_pilots, 'The pilots number exceed the maximum allowed');
+    IF (UnderMaxLimit(new_vehicles_num, NEW.max_vehicles))
 
-    SELECT
-
-    -- IF new_vehicles_num > NEW.max_vehicles THEN
-    --     SIGNAL SQLSTATE '45000'
-    --         SET MESSAGE_TEXT = 'The vehicles number exceed the maximum allowed';
-    -- END IF;
-
-    -- IF new_pilots_num > NEW.max_pilots THEN
-    --     SIGNAL SQLSTATE '45000'
-    --         SET MESSAGE_TEXT = 'The pilots number exceed the maximum allowed';
-    -- END IF;
+    SELECT UnderMaxLimit(new_vehicles_num, NEW.max_vehicles, 'The vehicles number exceed the maximum allowed');
+    SELECT UnderMaxLimit(new_pilots_num, NEW.max_pilots, 'The pilots number exceed the maximum allowed');
 
 END //
 DELIMITER ;
