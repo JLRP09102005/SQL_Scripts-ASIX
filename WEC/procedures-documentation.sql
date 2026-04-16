bbf2620cdd90e9602bbf1e306c364028eb38b82a
Jose Luis Ruiz Perez <jlrp09102005@gmail.com>
2026-04-16 12:59:45 +0200

    save

--- WEC/procedures-documentation.sql
+++ WEC/procedures-documentation.sql
@@ -29,44 +29,94 @@ CREATE PROCEDURE AddPilotPenalty ( IN penalty_type VARCHAR(20), IN penalty_value
 NOT DETERMINISTIC
 MODIFIES SQL DATA
 BEGIN
-    DECLARE affected_rows INT DEFAULT 0;
+    #Declarar una variable "affected_rows" de tipo INT y por defecto con valor 0
+
+    #Declarar un exit handler para un sqlexception
+        #Un rollback para que la transaccion la cual contiene el error vuelva a poner los valores anteriores a los cambios
+        #Un resignal para reanunciar el motivo del evento de error
+
+    ##Comienzo de la transaccion
+
+    #Comprobar el estado del penalty (POINTS,TIME,DNF)
+    #-Se calcula los puntos de resultado restado al del penalty y se actualiza
+    #-Despues de calcular e insertar, insertar en en la variable affected_rows cuantos registros fueron afectados
+    #-En el caso de que no sea correcto el estado de penalty enviar un signal sqlstate 45000
+
+    #Comprobar si affected_rows tiene registros afectados
+    #-En el caso de no tenerlosenviar un signal sqlstate 45000 avisando de esto
+
+    ##Final de transaccion
+
+END //
+
+CREATE PROCEDURE GetPenaltyBasicInfo ( IN id_penalty INT, OUT penalty_type VARCHAR(20), OUT penalty_applies VARCHAR(20), OUT penalty_value DECIMAL(7,2) )
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+
+    #Crear un select para sacar de la tabla de penalties la informacion de un penalty en especifico
+
+END //
+
+CREATE PROCEDURE GetResultsForeignInfo ( IN result_id INT, OUT vehicle_id INT, OUT team_id INT, OUT race_id INT )
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+    
+    #Crear un select para sacar de la tabla de resultados la informacion de un resultado en especifico
+
+END //
+
+CREATE PROCEDURE ProcessResultPenalty ( IN id_penalty INT, IN id_result INT )
+DETERMINISTIC
+MODIFIES SQL DATA
+BEGIN
+
+    #Declarar una variable "penalty_type" de tipo VARCHAR(20) por defecto con valor 'POINTS'
+    #Declarar una variable "penalty_applies" de tipo VARCHAR(20) por defecto con valor 'PILOT'
+    #Declarar una variable "penalty_value" de tipo DECIMAL(7,2) por defecto con valor 0
+    #Declarar una variable "vehicle_id" de tipo INT por defecto con valor NULL
+    #Declarar una variable "team_id" de tipo INT por defecto con valor NULL
+    #Declarar una variable "race_id" de tipo INT por defecto con valor NULL
+
+    #LLamada al procedimiento GetPenaltyBasicInfo
+    #LLamada al procedimiento GetResultsForeignInfo
+
+    #Condicional if que compruebe si la penalizacion se aplica al piloto o al equipo
+    #-En el caso de que se aplique al piloto se llama al procedimiento AddPilotPenalty
+    #-En el caso de que se aplique al equipo se llama al procedimiento AddTeamPenalty
+
+END //
+
+CREATE PROCEDURE AddResultPoints(IN race_id INT, IN result_id INT, IN result_points TINYINT)
+DETERMINISTIC
+MODIFIES SQL DATA
+BEGIN
+
+    #Declarar un exit handler para SQLEXCEPTION
+        #Hacer un rollback a cualquier transaccion ocurrida
+        #Hacer un resignal para volver a lanzar el evento de error
+    
+    ##Comenzar la transaccion
+
+    #Actualizar la base de datos resultados con la informacion 
+
+
+
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
         ROLLBACK;
         RESIGNAL;
     END;
-    
+
     START TRANSACTION;
-    IF (penalty_type = 'POINTS') THEN
-        UPDATE results res
-        SET res.penalty_points_pilot = GREATEST(0, res.penalty_points_pilot - FLOOR(penalty_value))
-        WHERE res.id_result = result_id;
-
-        SET affected_rows = ROW_COUNT();
-
-    ELSEIF (penalty_type = 'TIME') THEN
-        UPDATE results res
-        SET res.penalty_time = ADDTIME(res.penalty_time, SEC_TO_TIME(penalty_value))
-        WHERE res.id_result = result_id;
-
-        SET affected_rows = ROW_COUNT();
-
-    ELSEIF (penalty_type = 'DNF' || penalty_type = 'DSQ') THEN
-        UPDATE results res
-        SET 
-            res.penalty_time = SEC_TO_TIME(0),
-            res.penalty_points_pilot = 0
-        WHERE res.id_result = result_id;
-
-        SET affected_rows = ROW_COUNT();
-
-    ELSE
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid penalty_type';
-    END IF;
-
-    IF (CheckAfectedRowsCount(affected_rows)) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No affected rows';
-    END IF;
-
+    UPDATE results res SET
+        res.base_points_team = result_points,
+        res.base_points_pilot = result_points,
+        res.penalty_points_team = result_points,
+        res.penalty_points_pilot = result_points
+    WHERE 
+        res.id_race = race_id AND
+        res.id_result = result_id;
     COMMIT;
 END //
